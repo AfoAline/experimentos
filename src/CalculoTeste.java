fc53564eb853773da018b660c512cbcc352f3a19
Aline Freitas Oliveira <aoliveira@scopus.com.br>
2020-03-05 10:13:21 -0300

    Alterando a classe de CalculoTeste

--- src/CalculoTeste.java
+++ src/CalculoTeste.java
@@ -3,8 +3,8 @@ import junit.framework.TestCase;
 public class CalculoTeste extends TestCase{
 	public void testExecutaCalculo() {
 		float PassaValor1 = 10;
-		float PassaValor2 = 5;
-		float RetornoEsperado = 15;
+		float PassaValor2 = 10;
+		float RetornoEsperado = 20;
 		float RetornoFeito = Calculo.ExecutaCalculo(PassaValor1, PassaValor2);
 		assertEquals(RetornoEsperado, RetornoFeito, 0);
 		
